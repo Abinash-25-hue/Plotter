ee4d4eceb1e1d5094561921450c633ee0f160365
Abinash-25-hue <shivanand1.july.22@gmail.com>
2026-07-23 23:59:47 +0530

    Feat : added the code to store data and exit 'read' mode
    Added the code to store data from the files and exit the read mode if a row has more than 2 values which is invalid.

--- line_plotter/line_plot.py
+++ line_plotter/line_plot.py
@@ -5,4 +5,15 @@ x = []
 y = []
 
 with open("example1.csv", "r") as file :
-    read = csv.reader(file)
+    reader = csv.reader(file)
+
+    for row in reader:
+
+        #print(len(row))
+        if (len(row) > 2) :
+            pass
+
+        x += row[0]
+        y += row[1]   
+
+    pass
